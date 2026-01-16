e9933fcb6fd14671e8e5e11a5408aaba5df0b4b6
BrntChip <q.rakay@gmail.com>
2026-01-16 16:16:48 -0700

    Lab Participation

--- ListyCity/code/app/src/main/java/com/example/listycity/MainActivity.java
+++ ListyCity/code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.listycity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,10 @@ public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> datalist;
 
+    EditText cityInput;
+    Button add, delete, confirm;
+    int selectedPosition = -1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        TextView title = findViewById(R.id.title);
+        title.setText("ListyCity");
+
         cityList = findViewById(R.id.city_list);
 
         String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Delhi"};
@@ -39,5 +49,41 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, datalist);
         cityList.setAdapter(cityAdapter);
+
+        cityInput = findViewById(R.id.city_input);
+        add = findViewById(R.id.add);
+        delete = findViewById(R.id.delete);
+        confirm = findViewById(R.id.confirm);
+
+        add.setOnClickListener(v -> {
+            cityInput.setEnabled(true);
+            confirm.setEnabled(true);
+            cityInput.requestFocus();
+        });
+
+        confirm.setOnClickListener(v -> {
+            String cityname = cityInput.getText().toString().trim();
+
+            if (!cityname.isEmpty()) {
+                datalist.add(cityname);
+                cityAdapter.notifyDataSetChanged();
+
+                cityInput.setText("");
+                cityInput.setEnabled(false);
+                confirm.setEnabled(false);
+            }
+        });
+
+        cityList.setOnItemClickListener((parent, view, position, id) -> {
+            selectedPosition = position;
+        });
+
+        delete.setOnClickListener(v -> {
+            if (selectedPosition != -1) {
+                datalist.remove(selectedPosition);
+                cityAdapter.notifyDataSetChanged();
+                selectedPosition = -1;
+            }
+        });
     }
 }
